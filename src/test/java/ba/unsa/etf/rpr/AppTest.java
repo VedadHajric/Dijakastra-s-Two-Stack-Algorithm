f36ecf0ffbe6ff288900c914d17a0f0d0ca3fd03
VedadHajric <vhajric1@etf.unsa.ba>
2022-11-19 16:33:42 +0100

    Finalna verzija unit testova za App klasu naseg projekta

--- src/test/java/ba/unsa/etf/rpr/AppTest.java
+++ src/test/java/ba/unsa/etf/rpr/AppTest.java
@@ -1,20 +1,75 @@
 package ba.unsa.etf.rpr;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Unit test for simple App.
+ * Unit testovi za nas projekat odnosno zadacu za klasu App
  */
-public class AppTest 
+public class AppTest
 {
     /**
-     * Rigorous Test :-)
+     * Testiranje ispravnosti ulaza, prvi test je primjer iz postavke zadace ( 1 + ( 5 * 20 ) )
      */
     @Test
-    public void shouldAnswerWithTrue()
+    public void primjerIzZadace1()
     {
-        assertTrue( true );
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 1 + ( 5 * 20 ) )"}));
     }
+
+    /**
+     * Testiranje ispravnosti ulaza, primjer bez operanada  ( - ( / ) )
+     */
+    @Test
+    public void primjerIzZadace2()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( - ( / ) )"}));
+    }
+
+    /**
+     * Testiranje ispravnosti ulaza, primjer u kojem nisu sve otvorene zagrade zatvorene  ( 3 + ( 4 - 1 )
+     */
+    @Test
+    public void primjerIzZadace3()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 3 + ( 4 - 1 )"}));
+    }
+
+    /**
+     * Testiranje ispravnosti ulaza, primjer u kojem nedostaje prva zagrada   9 - ( 8 / 2 ) )
+     */
+    @Test
+    public void primjerIzZadace4()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{" 9 - ( 8 / 2 ) )"}));
+    }
+
+
+    /**
+     * Testiranje ispravnosti ulaza, primjer  u kojem nedostaje operator  ( 4 ( 6 + 3 ) )
+     */
+    @Test
+    public void primjerIzZadace5()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 4 ( 6 + 3 ) )"}));
+    }
+
+    /**
+     * Testiranje ispravnosti ulaza, primjer u kojem imamo karakter koji nije u zadatim  ( 3 ! ( 5 + 1 ) )
+     */
+    @Test
+    public void primjerIzZadace6()
+    {
+        assertEquals("Ilegalan izraz", App.main(new String[]{"( 3 ! ( 5 + 1 ) )"}));
+    }
+
+    /**
+     * Za posljednji test, jedan integracijski test
+     */
+    @Test
+    public void integration()
+    {
+        assertEquals("101.0", App.main(new String[]{"( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )"}));
+    }
+
 }
